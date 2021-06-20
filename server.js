3231761ca9bf5c079e84a5596d5d431d952d2c5f
piyanshujain <piyanshujain02@gmail.com>
2021-06-20 18:39:04 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express'),
+      app     = express(),
+      server  = require('http').Server(app);
+      const {v4 : uuidv4} = require('uuid');
+
+
+app.set('view engine' , 'ejs');
+app.get('/', (req,res)=>{
+  var id=uuidv4();
+  res.redirect('/$'+id);
+})
+
+
+app.get('/:room' , (req, res)=>{
+  res.render('room' , { roomId:req.params.room});
+})
+
+
+
+      server.listen(3030);
