d6abce4d09ac45391fb900ce1b12635e05fb1dd1
piyanshujain <piyanshujain02@gmail.com>
2021-07-13 01:35:16 +0530

    Adde invite participant feature

--- public/script_chat-room.js
+++ public/script_chat-room.js
@@ -9,9 +9,11 @@ $( '.friend-drawer--onhover' ).on( 'click',  function() {
 function toggleNav() {
   const wid=document.getElementById("mySlidebar").style.width;
   if(wid=="0px"){
+    $('.attendees').css("color","black");
     document.getElementById("mySlidebar").style.width = "200px";
      document.getElementById("main-side").style.marginRight = "250px";
   }else{
+    $('.attendees').css("color","white");
     document.getElementById("mySlidebar").style.width = "0";
     document.getElementById("main-side").style.marginRight = "0";
   }
@@ -19,6 +21,7 @@ function toggleNav() {
   
   /* Set the width of the slidebar to 0 and the left margin of the page content to 0 */
   function closeNav() {
+    $('.attendees').css("color","white");
     document.getElementById("mySlidebar").style.width = "0";
     document.getElementById("main-side").style.marginRight = "0";
   }
@@ -27,3 +30,18 @@ function toggleNav() {
   function startCall(){
     window.location.href = "/"+room_i;
   }
+
+
+  var $temp = $("<input>");
+var $url = $(location).attr('href');
+const copy_link=()=>{
+$("body").append($temp);
+$temp.val($url).select();
+document.execCommand("copy");
+$temp.remove();
+$('.invite').css("color","green");
+}
+
+const outFunc=()=>{
+  $('.invite').css("color","white");
+}
